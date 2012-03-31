35b6cf3ac91ff7d4e1a624f2440232e20adca2e7
Thor Smith <smith777@purdue.edu>
2012-03-31 11:49:07 -0400

    modified source code file to fit into the template given for our project

--- src/sillydigijocks.c
+++ src/sillydigijocks.c
@@ -1,8 +1,78 @@
+/***********************************************************************
+; ECE 362 - Mini-Project ASM Source File - Spring 2012                     
+;***********************************************************************
+;	 	   			 		  			 		  		
+; Team ID: < ? >
+;
+; Project Name: < ? >
+;
+; Team Members:
+;
+;   - Team/Doc Leader: < ? >      Signature: ______________________
+;   
+;   - Software Leader: < ? >      Signature: ______________________
+;
+;   - Interface Leader: < ? >     Signature: ______________________
+;
+;   - Peripheral Leader: < ? >    Signature: ______________________
+;
+;
+; Academic Honesty Statement:  In signing above, we hereby certify that we 
+; are the individuals who created this HC(S)12 source file and that we have
+; not copied the work of any other student (past or present) while completing 
+; it. We understand that if we fail to honor this agreement, we will receive 
+; a grade of ZERO and be subject to possible disciplinary action.
+;
+;***********************************************************************
+;
+; The objective of this Mini-Project is to .... < ? >
+;
+;
+;***********************************************************************
+;
+; List of project-specific success criteria (functionality that will be
+; demonstrated):
+;
+; 1.
+;
+; 2.
+;
+; 3.
+;
+; 4.
+;
+; 5.
+;
+;***********************************************************************
+;
+;  Date code started: < ? >
+;
+;  Update history (add an entry every time a significant change is made):
+;
+;  Date: < ? >  Name: < ? >   Update: < ? >
+;
+;  Date: < ? >  Name: < ? >   Update: < ? >
+;
+;  Date: < ? >  Name: < ? >   Update: < ? >
+;
+;
+;***********************************************************************/
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
+#include <mc9s12c32.h>
 
 
+// Define threshold voltages
+#define THRESHVERTUP 3.5
+#define THRESHVERTDO 1.5
 
+// All funtions after main should be initialiezed here
+char inchar(void);
+void outchar(char x);
+void displaySplash(void);
+
+
+// Variable declarations  	   			 		  			 		       
 // GLOBAL SCREEN BUFFER (2304 pixels)
 char screen[1152];
 
@@ -12,35 +82,139 @@ char joy1hor = 0;
 char joy0vert = 0;
 char joy1vert = 0;
 
-// Define threshold voltages
-#define THRESHVERTUP 3.5
-#define THRESHVERTDO 1.5
+// ASCII character definitions
+//int CR = 0x0D;//Return       ***** Use '\r' instead and use '\n' for newline
+	 	   		
+/***********************************************************************
+Initializations
+***********************************************************************/
+void  initializations(void) {
 
+//; Set the PLL speed (bus clock = 24 MHz)
+  CLKSEL = CLKSEL & 0x80; //; disengage PLL from system
+  PLLCTL = PLLCTL | 0x40; //; turn on PLL
+  SYNR = 0x02;            //; set PLL multiplier
+  REFDV = 0;              //; set PLL divider
+  while (!(CRGFLG & 0x08)){  }
+  CLKSEL = CLKSEL | 0x80; //; engage PLL
+
+// Disable watchdog timer (COPCTL register)
+  COPCTL = 0x40   ; //COP off; RTI and COP stopped in BDM-mode
+
+// Initialize asynchronous serial port (SCI) for 9600 baud, no interrupts
+  SCIBDH =  0x00; //set baud rate to 9600
+  SCIBDL =  0x9C; //24,000,000 / 16 / 156 = 9600 (approx)  dec=26
+  SCICR1 =  0x00; //$9C = 156
+  SCICR2 =  0x0C; //initialize SCI for program-driven operation
+  DDRB   =  0x10; //set PB4 for output mode
+  PORTB  =  0x10; //assert DTR pin on COM port
+            
+// Initialize interrupts
+	      
+	      
+}
+
+	 		  			 		  		
+/***********************************************************************
+Main
+***********************************************************************/
 void main(void) {
-  /* put code here */
-
-	// Load/Display Spalsh Screen
-
-	/* Main Loop. We never need to exit from here. */
-	while(1)
-	{
-			// Display Menu Screen
-			// Check for Menu Selection
-			// Use case statement to branch to appropriate
-			// Sub Function
-			// Menu:
-			//	Select Character
-			//	Select Field
-			//	Start Match
-	}
-	
-
-	/* Don't modify below here. */
+  DisableInterrupts;
+	initializations(); 		  			 		  		
 	EnableInterrupts;
 
+//////////////////////////////////////////////////////////////
+//;  START OF CODE FOR Spring 2012 MINI-PROJECT
+//////////////////////////////////////////////////////////////
+  
+	// Load/Display Spalsh Screen
+	displaySplash();
 
   for(;;) {
+   // write code here (Insert Code down here because we need an infinite loop.)
+
+	 // We don't need the watchdog timer, but I don't think it can hurt to feed it anyway.
+	 // The watchdog was disabled in the initialization code.
+
+	// Display Menu Screen
+	// Check for Menu Selection
+	// Use case statement to branch to appropriate
+	// Sub Function
+	// Menu:
+	//	Select Character
+	//	Select Field
+	//	Start Match
+    
     _FEED_COP(); /* feeds the dog */
   } /* loop forever */
   /* please make sure that you never leave main */
 }
+
+
+
+
+/***********************************************************************                       
+; RTI interrupt service routine: RTI_ISR
+;
+; Make sure you add it to the interrupts vector table (VECTOR 7 RTI_ISR) 
+; under: Project Settings/Linker Files/Project.PRM 		  			 		  		
+;***********************************************************************/
+interrupt 7 void RTI_ISR(void)
+{
+  	// set CRGFLG bit 
+  	CRGFLG = CRGFLG | 0x80; 
+// No need to add anything in the .PRM file, the interrupt number is included above
+}
+
+/***********************************************************************                       
+;  TIM interrupt service routine  (If needed!)
+;
+; Make sure you add it to the interrupts vector table (VECTOR 15 TIM_ISR) 
+; under: Project Settings/Linker Files/Project.PRM 					 		  			 		  		
+;***********************************************************************/
+interrupt 15 void TIM_ISR(void)
+{
+  // set TFLG1 bit 
+ 	TFLG1 = TFLG1 | 0x80; 
+// No need to add anything in the .PRM file, the interrupt number is included above
+}
+
+/*
+;***********************************************************************
+; Name:         displaySplash
+; Description:  Displays Splash screen for the game and gives the user
+;								time to see it.
+;***********************************************************************/
+void displaySplash(void);
+{
+}
+
+/***********************************************************************
+; Character I/O Library Routines for 9S12C32
+;***********************************************************************
+; Name:         inchar
+; Description:  inputs ASCII character from SCI serial port and returns it
+;***********************************************************************/
+char  inchar(void) {
+  /* receives character from the terminal channel */
+        while (!(SCISR1 & 0x20)); /* wait for input */
+    return SCIDRL;
+ 
+}
+
+/***********************************************************************
+; Name:         outchar
+; Description:  outputs ASCII character passed in outchar()
+;                  to the SCI serial port
+;***********************************************************************/
+void outchar(char ch) {
+  /* sends a character to the terminal channel */
+    while (!(SCISR1 & 0x80));  /* wait for output buffer empty */
+    SCIDRL = ch;
+}
+
+
+
+/***********************************************************************
+; ECE 362 - Mini-Project ASM Source File - Spring 2012                         
+;***********************************************************************/
