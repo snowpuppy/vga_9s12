641d9495b543f180584c2609f546f40da9cc89d7
Thor Smith <thor@mjolnir-III.(none)>
2012-04-18 22:44:43 -0400

    moved image data files to their own directory

--- images/convert.py
+++ images/convert.py
@@ -55,19 +55,23 @@ theimage = Image.open(sys.argv[1])
 
 # open the file to write stuff to
 outfilename = sys.argv[1]
+if outfilename[0] == ".":
+    outfilename = outfilename[2:]
 outfilename = outfilename[0:-3] + 'h'
 outfile = open(outfilename,'w') 
 # get array of pixel values from the image
 pixels = theimage.load()
 imwidth = theimage.size[0]
 imheigh = theimage.size[1]
+imsize = (imwidth/2)*imheigh
 
 # write comments and c-style syntax
 outfile.write('// This is an automatically generated file.\n')
 outfile.write('// This file is generated for %s.png by convert.py\n\n' % outfilename[0:-2] )
 outfile.write('#ifndef %s_H\n\n' % outfilename[0:-2].upper() )
 outfile.write('#define %s_H\n\n' % outfilename[0:-2].upper() )
-outfile.write('const unsigned char image_%s[%i][%i] = {\n' % (outfilename[0:-2], imheigh, imwidth/2 ) )
+#outfile.write('const unsigned char image_%s[%i][%i] = {\n' % (outfilename[0:-2], imheigh, imwidth/2 ) )
+outfile.write('const unsigned char image_%s[%i] = {\n' % (outfilename[0:-2], imsize) )
 
 for h in range(0,imheigh):
     for w in range(0,imwidth,2):
